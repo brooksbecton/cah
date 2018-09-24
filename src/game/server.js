3cec845d5578eaf2bc4b1c41ca90a2c6fdc4320e
Brooks Becton <broabect@ut.utm.edu>
2018-09-23 21:05:47 -0500

    Route all requests to client for react router

--- src/game/server.js
+++ src/game/server.js
@@ -2,11 +2,18 @@ import path from "path";
 import KoaStatic from "koa-static";
 import { Server } from "boardgame.io/server";
 import { cah } from "./index";
+import Router from "koa-router";
+import send from "koa-send";
 
 const server = Server({ games: [cah] });
-const buildPath = path.join(__dirname, "../dist");
+const router = new Router();
 
-server.app.use(KoaStatic(buildPath));
+// Forwarding all requests to base index for client routing
+router.get("/(.*)", async (ctx) => {
+    await send(ctx,"dist/index.html")
+});
+
+server.app.use(router.routes());
 server.run(5555);
 
 console.log("Server Started");
