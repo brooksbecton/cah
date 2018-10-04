e88875e7cbe84352d244666e1ab11df08ad9df60
Brooks Becton <bbecton@Brooks-Becton-MacBook-Pro.local>
2018-10-03 20:53:07 -0500

    Add env file

--- src/game/server.js
+++ src/game/server.js
@@ -4,17 +4,18 @@ import { Server } from "boardgame.io/server";
 import { cah } from "./index";
 import Router from "koa-router";
 import send from "koa-send";
+import dotenv from 'dotenv';
 
 const server = Server({ games: [cah] });
 const router = new Router();
 const buildPath = path.join(__dirname, "../dist");
+dotenv.config();
 // Forwarding all requests to base index for client routing
 server.app.use(KoaStatic(buildPath));
 router.get("/(.*)", async ctx => {
   await send(ctx, "dist/index.html");
 });
-
 server.app.use(router.routes());
-server.run(5555);
+server.run(Number(process.env.PORT));
 
-console.log("Server Started");
+console.log("Server Started on " + process.env.PORT);
