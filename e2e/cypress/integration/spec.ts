9cc7e59d688f44354011062445c99995f038c08d
Brooks Becton <bbecton@babelstreet.com>
2020-04-06 20:26:03 -0400

    Update spec to user API calls and add vote test

--- e2e/cypress/integration/spec.ts
+++ e2e/cypress/integration/spec.ts
@@ -1,4 +1,4 @@
-import { url } from "./../../../client/src/config/url";
+import { serverUrl } from "./../../../client/src/config/serverUrl";
 import { getGames } from "./../../../client/src/game/utils/getGames";
 import { IGame } from "./../../../client/src/game/game/types";
 
@@ -7,79 +7,95 @@ function getByTestId(id: string) {
 }
 
 function createGame(numPlayers: number = 2, setupData: Partial<IGame> = {}) {
-  return cy.request("POST", `${url}/games/default/create`, {
+  return cy.request("POST", `${serverUrl}/games/default/create`, {
     numPlayers,
-    setupData
+    setupData,
   });
 }
 
+function joinGame(gameId: string, playerName: string) {
+  return cy.request("POST", `${serverUrl}/games/default/${gameId}/join`, {
+    playerName,
+    playerID: 0,
+  });
+}
+
+const clientUrl = `localhost:3000`;
+
 function goHome() {
-  cy.visit(`localhost:3000/cah`);
+  cy.visit(clientUrl);
+}
+
+function goToJoin(gameId: string = "") {
+  cy.visit(`${clientUrl}/join${gameId && `/${gameId}`}`);
 }
 
 describe("Game", () => {
   beforeEach(() => {
     cy.viewport(1920, 1080);
+  });
+
+  it("navigates to 'Join' pages from 'Home' page", () => {
     goHome();
+
+    cy.get(getByTestId("NavigateToJoin")).click();
+
+    cy.url().should("include", "/join");
   });
 
-  it.skip("adds game id to #gameID input", () => {
-    goHome();
-    cy.get(getByTestId("createGameButton")).click();
-
-    cy.get(getByTestId("gameId")).contains(/^.*/);
-  });
-
-  it.skip("lets players create a game", () => {
-    cy.get(getByTestId("createGameButton")).click();
-    cy.get(getByTestId("gameId")).should($input => {
-      const gameId = $input.val();
-      getGames().then(({ rooms }) => {
-        const gameIds = rooms.map(r => r.gameID);
-        expect(gameIds).should("include", String(gameId));
-      });
-    });
-  });
-  it.skip("lets players join games", () => {
-    createGame().then(response => {
-      const gameID = response.body.gameID;
-      cy.get(getByTestId("gameId")).type(String(gameID));
-      cy.get(getByTestId("playerId")).type("1");
-      cy.get(getByTestId("playerName")).type("Bob");
-
-      cy.get(getByTestId("joinGame")).click();
-      cy.url().should("include", gameID);
-    });
-  });
-
-  it("allows players to play cards", () => {
-    // Create a gmae
+  it("can join game through 'Join' page", () => {
     createGame(2).then(({ body: { gameID } }) => {
       // P1 Join Game
-      cy.get(getByTestId("NavigateToJoin")).click();
+      goToJoin();
+
       cy.get(getByTestId("gameId")).type(gameID);
       cy.get(getByTestId("playerName")).type("Brooks");
       cy.get(getByTestId("joinGame")).click();
 
       cy.url().should("include", gameID);
-
-      cy.get(getByTestId("start-game-button")).click();
-
-      goHome();
-
-      cy.get(getByTestId("NavigateToJoin")).click();
-      cy.get(getByTestId("gameId")).type(gameID);
-      cy.get(getByTestId("playerName")).type("Hope");
-      cy.get(getByTestId("joinGame")).click();
-
-      const firstCardSelector = `${getByTestId(`players-hand`)} li:first-child`;
-      cy.get(firstCardSelector)
-        .focus()
-        .type(" ")
-        .type("{rightarrow}")
-        .type(" ");
-      cy.get(getByTestId("played-card-list")).find("li");
     });
   });
-  it("only allows czar to vote on cards");
+  it("pre-fills gameId from URL", () => {
+    goToJoin("abc123");
+    cy.get(getByTestId("gameId")).should("have.value", "abc123");
+  });
+
+  it("allows players to play cards", () => {
+    const playState: Partial<IGame> = {
+      gameStarted: true,
+    };
+
+    // Create a game
+    createGame(2, playState).then(({ body: { gameID } }) => {
+      // First Player Join
+      joinGame(gameID, "Brooks").then(({ body: { playerCredentials } }) => {
+        cy.visit(`${clientUrl}/cah/game/${gameID}/${playerCredentials}/1`);
+
+        const firstCardSelector = `${getByTestId(
+          `players-hand`
+        )} li:first-child`;
+        cy.get(firstCardSelector)
+          .focus()
+          .type(" ")
+          .type("{rightarrow}")
+          .type(" ");
+        cy.get(getByTestId("played-card-list")).find("li");
+      });
+    });
+  });
+  it("only allows czar to vote on cards", () => {
+    const voteState: Partial<IGame> = {
+      gameStarted: true,
+      currentCzarID: 0,
+      playedCards: [{ text: "Winner Winner Chicken Dinner", playerID: "1" }],
+    };
+
+    createGame(2, voteState).then(({ body: { gameID } }) => {
+      joinGame(gameID, "Brooks").then(({ body: { playerCredentials } }) => {
+        cy.visit(`${clientUrl}/cah/game/${gameID}/${playerCredentials}/0`);
+
+        cy.get(getByTestId("played-card-list")).find("li").click();
+      });
+    });
+  });
 });
