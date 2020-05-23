b67cd66cbbc281cbc8537098260e2fac461672c7
Brooks Becton <bbecton@babelstreet.com>
2020-05-23 09:52:49 -0400

    Add Sentry

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import * as Sentry from '@sentry/browser';
 
 import { BaseRouter } from "./ui/pages";
 import "./App.css";
+
+Sentry.init({
+  dsn:
+    "https://9461a3e2e08d4c95a0bb18665b905122@o397091.ingest.sentry.io/5251273",
+});
+
 function App() {
   return <BaseRouter />;
 }
