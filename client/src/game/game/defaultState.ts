ce9b43f3fd1304ba2952019478aba9427b7272be
Brooks Becton <bbecton@babelstreet.com>
2020-05-23 09:31:00 -0400

    Temp only allow cards with pick of 1

--- client/src/game/game/defaultState.ts
+++ client/src/game/game/defaultState.ts
@@ -3,7 +3,14 @@ import { IGame } from "./types";
 
 export const defaultState: IGame = {
   isShowcasing: false,
-  blackCards: cards.blackCards,
+  /**
+   * @TODO
+   * I think all the logic works with picks > 1, but I need to 
+   * think about the UX around picking cards from the czar point of view. 
+   * 
+   * And how showcasing would work
+   */
+  blackCards: cards.blackCards.filter((card) => card.pick === 1),
   cardLimit: 10,
   currentBlackCard: { pick: 0, text: "" },
   currentCzarID: 0,
