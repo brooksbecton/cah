a67f4500122d6ba5779c49d245f35a843d0afef0
Brooks Becton <bbecton@babelstreet.com>
2020-05-23 10:07:54 -0400

    Add sentry to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 import { Server } from "boardgame.io/server";
-import { cah } from "./../client/src/game/game";
 import dotenv from "dotenv";
+import * as Sentry from '@sentry/node';
+
+import { cah } from "./../client/src/game/game";
 
 const server = Server({ games: [cah] });
 
 dotenv.config();
 
 const port = Number(process.env.PORT);
+
+Sentry.init({ dsn: 'https://b7c5fc6639b54250b329c849d3ddf7d2@o397091.ingest.sentry.io/5251277' });
+
 server.run({ port });
